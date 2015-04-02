921ee60a6e7b5690ca6266aea157b23f25a7da49
Cauê Waneck <waneck@gmail.com>
2015-04-02 14:26:41 -0300

    Update to latest Int64 (use low/high instead of getHigh/getLow)

--- org/bsonspec/MongoDate.hx
+++ org/bsonspec/MongoDate.hx
@@ -16,7 +16,7 @@ abstract MongoDate(Date) {
     {
         // double = high << 32 + low
         //    with  a << b = a*(1 << b)
-        return new MongoDate(Date.fromTime(POW32f*ms.getHigh() + unsigned((ms.getLow():Int))));
+        return new MongoDate(Date.fromTime(POW32f*ms.high + unsigned((ms.low:Int))));
     }
 
     @:to public inline function getTimeInt64():Int64
